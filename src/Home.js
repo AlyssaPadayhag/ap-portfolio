109c1c60a3adcd8770bc6623191a49f4891572da
Alyssa Padayhag <alyssapadayhag@gmail.com>
2023-01-09 14:51:30 -0500

    updated social icons to be in header

--- src/Home.js
+++ src/Home.js
@@ -8,12 +8,12 @@ function Home() {
         <div>
             <header className="App-header">
                 <h1>Alyssa Padayhag</h1>
-            </header>
             <p>
                 <a href="https://www.linkedin.com/in/alyssa-padayhag/" target="_blank"><i className="fa-brands fa-linkedin-in"></i></a>
                 <a href="https://github.com/AlyssaPadayhag" target="_blank"><i className="fa-brands fa-github"></i></a>
                 <a href="./files/resume-alyssa-padayhag-2023.pdf" download><i className="fa-regular fa-file-pdf"></i></a>
             </p>
+            </header>
         <div className="main-div">
             <AboutMe />
             <Skills />
