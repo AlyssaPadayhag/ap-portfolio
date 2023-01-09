852e0762f985f38b924329d01f275a385ab3175b
Alyssa Padayhag <alyssapadayhag@gmail.com>
2023-01-09 17:26:09 -0500

    rearranged content order

--- src/AboutMe/AboutMe.js
+++ src/AboutMe/AboutMe.js
@@ -5,12 +5,12 @@ function AboutMe() {
     return (
         <div className="about-skills">
             <h2>About Me</h2>
-            <div className="user one"></div>
-            <h3 className="avail">I am #OpenToWork!</h3>
             <p>
                 Hello! I am a full stack web developer with 3+ years of collective web dev experience.
                 I am also a recent grad from <a href="https://www.thinkful.com/bootcamp/web-development/" target="_blank">Thinkful's</a> intensive software engineering flex program.
             </p>
+            <div className="user one"></div>
+            <h3 className="avail">I am #OpenToWork!</h3>
         </div>
     );
 }
