a085fe897442968c28beda75d34962998134d584
Alyssa Padayhag <alyssapadayhag@gmail.com>
2023-01-09 10:41:49 -0500

    updated links

--- src/Home.js
+++ src/Home.js
@@ -10,9 +10,9 @@ function Home() {
                 <h1>Alyssa Padayhag</h1>
             </header>
             <p>
-                <a href="https://www.linkedin.com/in/alyssa-padayhag/"><i class="fa-brands fa-linkedin-in"></i></a>
-                <a href="https://github.com/AlyssaPadayhag"><i class="fa-brands fa-github"></i></a>
-                <a href="./files/resume.pdf" target="_blank">Resume</a>
+                <a href="https://www.linkedin.com/in/alyssa-padayhag/" target="_blank"><i className="fa-brands fa-linkedin-in"></i></a>
+                <a href="https://github.com/AlyssaPadayhag" target="_blank"><i className="fa-brands fa-github"></i></a>
+                <a href="./files/resume.pdf" target="_blank"><i className="fa-regular fa-file-pdf"></i></a>
             </p>
         <div className="main-div">
             <AboutMe />
