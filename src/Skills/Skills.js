705e981ea071534f9233d24c0c0b84d188098aa1
Alyssa Padayhag <alyssapadayhag@gmail.com>
2023-01-09 18:06:16 -0500

    updated skills

--- src/Skills/Skills.js
+++ src/Skills/Skills.js
@@ -8,11 +8,14 @@ function Skills() {
             <ul>
                 <li>HTML, CSS, flexbox, Bootstrap, SASS/SCSS</li>
                 <li>JavaScript, Node, React</li>
-                <li>Express, PostgreSQL, ElephantSQL, GraphQL, S3 </li>
+                <li>Express, PostgreSQL, ElephantSQL, GraphQL </li>
                 <li>AWS, Heroku, Vercel, Render</li>
                 <li>Git, GitHub, Git BASH</li>
-                <li>Mocha and Chai, Jest, Postman</li>
+                <li>Mocha and Chai, Jest</li>
+                <li>Postman</li>
                 <li>Data Structures and Algorithms</li>
+                <li>E-commerce: X-Cart, BigCommerce, Volusion, Shopify, Amazon Seller</li>
+                <li>Advertising</li>
             </ul>
         </div>
     );
