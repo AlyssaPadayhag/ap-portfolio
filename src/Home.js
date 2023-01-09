c9373654cc458161903fb3ce5efcd645f0a11bc9
Alyssa Padayhag <alyssapadayhag@gmail.com>
2023-01-09 17:25:43 -0500

    added pulse-shrink effect for social icons

--- src/Home.js
+++ src/Home.js
@@ -9,9 +9,9 @@ function Home() {
             <header className="App-header">
                 <h1>Alyssa Padayhag</h1>
             <p>
-                <a href="https://www.linkedin.com/in/alyssa-padayhag/" target="_blank"><i className="fa-brands fa-linkedin-in"></i></a>
-                <a href="https://github.com/AlyssaPadayhag" target="_blank"><i className="fa-brands fa-github"></i></a>
-                <a href="./files/resume-alyssa-padayhag-2023.pdf" download><i className="fa-regular fa-file-pdf"></i></a>
+                <a href="https://www.linkedin.com/in/alyssa-padayhag/" target="_blank" className="hvr-pulse-shrink"><i className="fa-brands fa-linkedin-in hvr-icon"></i></a>
+                <a href="https://github.com/AlyssaPadayhag" target="_blank" className="hvr-pulse-shrink"><i className="fa-brands fa-github hvr-icon"></i></a>
+                <a href="./files/resume-alyssa-padayhag-2023.pdf" download className="hvr-pulse-shrink"><i className="fa-regular fa-file-pdf hvr-icon"></i></a>
             </p>
             </header>
         <div className="main-div">
