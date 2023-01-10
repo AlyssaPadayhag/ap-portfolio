325f46ee8dda0bafcaba08129cd78c28338a8d43
Alyssa Padayhag <alyssapadayhag@gmail.com>
2023-01-10 14:31:04 -0500

    removed social icon animation

--- src/Home.js
+++ src/Home.js
@@ -10,9 +10,9 @@ function Home() {
             <header className="App-header">
                 <h1>Alyssa Padayhag</h1>
             <p>
-                <a href="https://www.linkedin.com/in/alyssa-padayhag/" target="_blank" className="hvr-pulse-shrink"><i className="fa-brands fa-linkedin-in fa-fade hvr-icon"></i></a>
-                <a href="https://github.com/AlyssaPadayhag" target="_blank" className="hvr-pulse-shrink"><i className="fa-brands fa-github fa-fade hvr-icon"></i></a>
-                <a href={pdf} target="_blank" className="hvr-pulse-shrink"><i className="fa-regular fa-file-pdf fa-fade hvr-icon"></i></a>
+                <a href="https://www.linkedin.com/in/alyssa-padayhag/" target="_blank" className="hvr-pulse-shrink"><i className="fa-brands fa-linkedin-in hvr-icon"></i></a>
+                <a href="https://github.com/AlyssaPadayhag" target="_blank" className="hvr-pulse-shrink"><i className="fa-brands fa-github hvr-icon"></i></a>
+                <a href={pdf} target="_blank" className="hvr-pulse-shrink"><i className="fa-regular fa-file-pdf hvr-icon"></i></a>
             </p>
             </header>
         <div className="main-div">
