1fc48bbd33ac34a4e175bb8462a6b33806621994
Alyssa Padayhag <alyssapadayhag@gmail.com>
2023-01-10 15:01:39 -0500

    added hvr pulse shrink on links

--- src/AboutMe/AboutMe.js
+++ src/AboutMe/AboutMe.js
@@ -7,7 +7,7 @@ function AboutMe() {
             <h2>About Me</h2>
             <p>
                 Hello! I am a full stack web developer with 3+ years of collective web dev experience.
-                I am also a recent grad from <a href="https://www.thinkful.com/bootcamp/web-development/" target="_blank">Thinkful's</a> intensive software engineering flex program.
+                I am also a recent grad from <a href="https://www.thinkful.com/bootcamp/web-development/" target="_blank" className="hvr-pulse-shrink">Thinkful's</a> intensive software engineering flex program.
             </p>
             <div className="user one"></div>
             <h3 className="avail">I am #OpenToWork!</h3>
--- src/Projects/Projects.js
+++ src/Projects/Projects.js
@@ -18,33 +18,33 @@ function Projects() {
                     <tbody>
                         <tr>
                             <td>Notes AWS App</td>
-                            <td><Link className="link-style" to="/notes" target="_blank"> <i class="fa-solid fa-arrow-up-right-from-square"></i> </Link></td>
-                            <td><a href="https://github.com/AlyssaPadayhag/ap-portfolio/blob/main/src/Notes.js"> <i class="fa-solid fa-arrow-up-right-from-square"></i> </a> </td>
+                            <td><Link to="/notes" target="_blank" className="link-style hvr-pulse-shrink"> <i class="fa-solid fa-arrow-up-right-from-square hvr-icon"></i> </Link></td>
+                            <td><a href="https://github.com/AlyssaPadayhag/ap-portfolio/blob/main/src/Notes.js"> <i class="fa-solid fa-arrow-up-right-from-square hvr-icon"></i> </a> </td>
                         </tr>
                         <tr>
                             <td>Restaurant Resrevation</td>
-                            <td><a href="https://ap-restaurant-reservation-frontend.onrender.com/dashboard"> <i class="fa-solid fa-arrow-up-right-from-square"></i> </a> </td>
-                            <td><a href="https://github.com/AlyssaPadayhag/ap-restaurant-reservation"> <i class="fa-solid fa-arrow-up-right-from-square"></i> </a> </td>
+                            <td><a href="https://ap-restaurant-reservation-frontend.onrender.com/dashboard" className="hvr-pulse-shrink"> <i class="fa-solid fa-arrow-up-right-from-square hvr-icon"></i> </a> </td>
+                            <td><a href="https://github.com/AlyssaPadayhag/ap-restaurant-reservation" className="hvr-pulse-shrink"> <i class="fa-solid fa-arrow-up-right-from-square hvr-icon"></i> </a> </td>
                         </tr>
                         <tr>
                             <td>WeLoveMovies</td>
-                            <td><a href="https://ap-welovemovies-backend.onrender.com/movies"> <i class="fa-solid fa-arrow-up-right-from-square"></i> </a> </td>
-                            <td><a href="https://github.com/AlyssaPadayhag/ap-restaurant-reservation"> <i class="fa-solid fa-arrow-up-right-from-square"></i> </a> </td>
+                            <td><a href="https://ap-welovemovies-backend.onrender.com/movies" className="hvr-pulse-shrink"> <i class="fa-solid fa-arrow-up-right-from-square hvr-icon"></i> </a> </td>
+                            <td><a href="https://github.com/AlyssaPadayhag/ap-restaurant-reservation" className="hvr-pulse-shrink"> <i class="fa-solid fa-arrow-up-right-from-square hvr-icon"></i> </a> </td>
                         </tr>
                         <tr>
                             <td>GrubDash</td>
                             <td></td>
-                            <td><a href="https://github.com/AlyssaPadayhag/ap-grubdash"> <i class="fa-solid fa-arrow-up-right-from-square"></i> </a> </td>
+                            <td><a href="https://github.com/AlyssaPadayhag/ap-grubdash" className="hvr-pulse-shrink"> <i class="fa-solid fa-arrow-up-right-from-square hvr-icon"></i> </a> </td>
                         </tr>
                         <tr>
                             <td>Simple Landing Page</td>
-                            <td><a href="https://ap-drinks-landing-page.onrender.com/"> <i class="fa-solid fa-arrow-up-right-from-square"></i> </a> </td>
-                            <td><a href="https://github.com/AlyssaPadayhag/ap-drinks-landing-page"> <i class="fa-solid fa-arrow-up-right-from-square"></i> </a> </td>
+                            <td><a href="https://ap-drinks-landing-page.onrender.com/" className="hvr-pulse-shrink"> <i class="fa-solid fa-arrow-up-right-from-square hvr-icon"></i> </a> </td>
+                            <td><a href="https://github.com/AlyssaPadayhag/ap-drinks-landing-page" className="hvr-pulse-shrink"> <i class="fa-solid fa-arrow-up-right-from-square hvr-icon"></i> </a> </td>
                         </tr>
                         <tr>
                             <td>Responsive Flexbox Design</td>
-                            <td><a href="https://ap-responsive-flexbox.onrender.com/"> <i class="fa-solid fa-arrow-up-right-from-square"></i> </a> </td>
-                            <td><a href="https://github.com/AlyssaPadayhag/ap-responsive-flexbox"> <i class="fa-solid fa-arrow-up-right-from-square"></i> </a> </td>
+                            <td><a href="https://ap-responsive-flexbox.onrender.com/" className="hvr-pulse-shrink"> <i class="fa-solid fa-arrow-up-right-from-square"></i> </a> </td>
+                            <td><a href="https://github.com/AlyssaPadayhag/ap-responsive-flexbox" className="hvr-pulse-shrink"> <i class="fa-solid fa-arrow-up-right-from-square"></i> </a> </td>
                         </tr>
                     </tbody>
                 </table>
