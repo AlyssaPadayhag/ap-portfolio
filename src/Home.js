bd2223b58ba41627f1179016c6b7d8986fed69f6
Alyssa Padayhag <alyssapadayhag@gmail.com>
2023-01-10 09:10:33 -0500

    fixed pdf file not showing

--- src/Home.js
+++ src/Home.js
@@ -2,6 +2,7 @@ import React from "react";
 import AboutMe from "./AboutMe/AboutMe";
 import Skills from "./Skills/Skills";
 import Projects from "./Projects/Projects";
+import pdf from "./files/resume-alyssa-padayhag-2023.pdf";
 
 function Home() {
     return(
@@ -11,7 +12,7 @@ function Home() {
             <p>
                 <a href="https://www.linkedin.com/in/alyssa-padayhag/" target="_blank" className="hvr-pulse-shrink"><i className="fa-brands fa-linkedin-in fa-fade hvr-icon"></i></a>
                 <a href="https://github.com/AlyssaPadayhag" target="_blank" className="hvr-pulse-shrink"><i className="fa-brands fa-github fa-fade hvr-icon"></i></a>
-                <a href="./files/resume-alyssa-padayhag-2023.pdf" download className="hvr-pulse-shrink"><i className="fa-regular fa-file-pdf fa-fade hvr-icon"></i></a>
+                <a href={pdf} target="_blank" className="hvr-pulse-shrink"><i className="fa-regular fa-file-pdf fa-fade hvr-icon"></i></a>
             </p>
             </header>
         <div className="main-div">
