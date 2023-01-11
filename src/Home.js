db646783a97b1a235583a752161aed2d05d516bd
Alyssa Padayhag <alyssapadayhag@gmail.com>
2023-01-11 12:40:10 -0500

    added title

--- src/Home.js
+++ src/Home.js
@@ -9,7 +9,8 @@ function Home() {
         <div>
             <header className="App-header">
                 <h1>Alyssa Padayhag</h1>
-                <h5 className="avail">I am #OpenToWork!</h5>
+                <h5 className="avail">I am a full-stack web developer</h5>
+                <h5 className="avail">#OpenToWork!</h5>
             <p>
                 <a href="https://www.linkedin.com/in/alyssa-padayhag/" target="_blank" className="hvr-pulse-shrink"><i className="fa-brands fa-linkedin-in hvr-icon"></i></a>
                 <a href="https://github.com/AlyssaPadayhag" target="_blank" className="hvr-pulse-shrink"><i className="fa-brands fa-github hvr-icon"></i></a>
