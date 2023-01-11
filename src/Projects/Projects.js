2e2ba4fbaca8b0157902ce0c51f2b099bec3e9f9
Alyssa Padayhag <alyssapadayhag@gmail.com>
2023-01-11 12:52:43 -0500

    updated project descriptions

--- src/Projects/Projects.js
+++ src/Projects/Projects.js
@@ -16,33 +16,52 @@ function Projects() {
                         </tr>
                     </thead>
                     <tbody>
+
                         <tr>
-                            <td>Notes AWS App</td>
-                            <td><Link to="/notes" target="_blank" className="link-style hvr-pulse-shrink"> <i class="fa-solid fa-arrow-up-right-from-square hvr-icon"></i> </Link></td>
-                            <td><a href="https://github.com/AlyssaPadayhag/ap-portfolio/blob/main/src/Notes.js"> <i class="fa-solid fa-arrow-up-right-from-square hvr-icon"></i> </a> </td>
-                        </tr>
-                        <tr>
-                            <td>Restaurant Resrevation</td>
+                            <td>
+                                Restaurant Reservation
+                                <p>Full-stack, CRUD, React, Express, PostGreSQL App</p>
+                            </td>
                             <td><a href="https://ap-restaurant-reservation-frontend.onrender.com/dashboard" className="hvr-pulse-shrink"> <i class="fa-solid fa-arrow-up-right-from-square hvr-icon"></i> </a> </td>
                             <td><a href="https://github.com/AlyssaPadayhag/ap-restaurant-reservation" className="hvr-pulse-shrink"> <i class="fa-solid fa-arrow-up-right-from-square hvr-icon"></i> </a> </td>
                         </tr>
                         <tr>
-                            <td>WeLoveMovies</td>
+                            <td>
+                                WeLoveMovies
+                                <p>Back-end, Node, Express, Knex, PostgreSQL, </p>
+                            </td>
                             <td><a href="https://ap-welovemovies-backend.onrender.com/movies" className="hvr-pulse-shrink"> <i class="fa-solid fa-arrow-up-right-from-square hvr-icon"></i> </a> </td>
                             <td><a href="https://github.com/AlyssaPadayhag/ap-restaurant-reservation" className="hvr-pulse-shrink"> <i class="fa-solid fa-arrow-up-right-from-square hvr-icon"></i> </a> </td>
                         </tr>
                         <tr>
-                            <td>GrubDash</td>
+                            <td>
+                                GrubDash
+                                <p>Back-end, Node, Express, REST API</p>
+                            </td>
                             <td></td>
                             <td><a href="https://github.com/AlyssaPadayhag/ap-grubdash" className="hvr-pulse-shrink"> <i class="fa-solid fa-arrow-up-right-from-square hvr-icon"></i> </a> </td>
                         </tr>
                         <tr>
-                            <td>Simple Landing Page</td>
+                            <td>
+                                Notes AWS App
+                                <p>Full-stack, Authentication, AWS: Amplify, S3</p>
+                            </td>
+                            <td><Link to="/notes" target="_blank" className="link-style hvr-pulse-shrink"> <i class="fa-solid fa-arrow-up-right-from-square hvr-icon"></i> </Link></td>
+                            <td><a href="https://github.com/AlyssaPadayhag/ap-portfolio/blob/main/src/Notes.js"> <i class="fa-solid fa-arrow-up-right-from-square hvr-icon"></i> </a> </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                Simple Landing Page
+                                <p>Front-end design</p>
+                            </td>
                             <td><a href="https://ap-drinks-landing-page.onrender.com/" className="hvr-pulse-shrink"> <i class="fa-solid fa-arrow-up-right-from-square hvr-icon"></i> </a> </td>
                             <td><a href="https://github.com/AlyssaPadayhag/ap-drinks-landing-page" className="hvr-pulse-shrink"> <i class="fa-solid fa-arrow-up-right-from-square hvr-icon"></i> </a> </td>
                         </tr>
                         <tr>
-                            <td>Responsive Flexbox Design</td>
+                            <td>
+                                Responsive Flexbox Design
+                                <p>Front-end responsive design</p>
+                            </td>
                             <td><a href="https://ap-responsive-flexbox.onrender.com/" className="hvr-pulse-shrink"> <i class="fa-solid fa-arrow-up-right-from-square"></i> </a> </td>
                             <td><a href="https://github.com/AlyssaPadayhag/ap-responsive-flexbox" className="hvr-pulse-shrink"> <i class="fa-solid fa-arrow-up-right-from-square"></i> </a> </td>
                         </tr>
