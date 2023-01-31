9b1002157b1dfedadd3214665ae72b3fc765c139
Alyssa Padayhag <alyssapadayhag@gmail.com>
2023-01-31 09:14:55 -0500

    updated to use tabs

--- src/Home.js
+++ src/Home.js
@@ -1,27 +1,29 @@
 import React from "react";
-import AboutMe from "./AboutMe/AboutMe";
-import Skills from "./Skills/Skills";
-import Projects from "./Projects/Projects";
 import pdf from "./files/alyssa_padayhag_resume.pdf";
+import BasicTabs from "./BasicTabs";
 
 function Home() {
     return(
         <div>
             <header className="App-header">
-                <h1>Alyssa Padayhag</h1>
-                <h5 className="avail">I am a full-stack web developer</h5>
-                <h5 className="avail">#OpenToWork!</h5>
-            <p>
+                <div className="flex-display">
+                    <div className="user one"></div>
+                    <div>
+                    <h1>Alyssa Padayhag</h1>
+                    <h5 className="avail">Full-stack web developer #OpenToWork!</h5>
+                    <p>
                 <a href="https://www.linkedin.com/in/alyssa-padayhag/" target="_blank" className="hvr-pulse-shrink"><i className="fa-brands fa-linkedin-in hvr-icon"></i></a>
                 <a href="https://github.com/AlyssaPadayhag" target="_blank" className="hvr-pulse-shrink"><i className="fa-brands fa-github hvr-icon"></i></a>
                 <a href={pdf} target="_blank" className="resume hvr-pulse-shrink">Resume</a>
             </p>
+                </div>
+                    
+                </div>
+                
+                
+
             </header>
-        <div className="main-div">
-            <AboutMe />
-            <Skills />
-            <Projects />
-        </div>
+            <BasicTabs />
         </div>
     );
 }
