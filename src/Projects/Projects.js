70caa04abcf2ce685fb849dcbadd35e7662b20b1
Alyssa Padayhag <alyssapadayhag@gmail.com>
2023-01-31 10:52:22 -0500

    updated projects

--- src/Projects/Projects.js
+++ src/Projects/Projects.js
@@ -5,12 +5,13 @@ import "./projects.scss";
 function Projects() {
     return(
         <div className="projects">
-            <h2>Projects</h2>
+            {/* <h2>Projects</h2> */}
             <div className="container">
                 <table>
                     <thead>
                         <tr>
                             <th>name</th>
+                            <th>description</th>
                             <th>demo</th>
                             <th>code</th>
                         </tr>
@@ -21,6 +22,10 @@ function Projects() {
                                 Thinkful Hackathon - Job Search App
                                 <p>Front-end, React, MUI, Bootstrap, Figma</p>
                             </td>
+                            <td>
+                                Worked with 2 designers and 2 other engineers and mimic a real world project.
+                                Prompt: to create a job search tool or app in 1 week
+                            </td>
                             <td><a href="https://thinkful-hackathon.onrender.com/" className="hvr-pulse-shrink" target="blank"> <i class="fa-solid fa-arrow-up-right-from-square hvr-icon"></i> </a> </td>
                             <td><a href="https://github.com/japyles/thinkful-hackathon" className="hvr-pulse-shrink" target="blank"> <i class="fa-solid fa-arrow-up-right-from-square hvr-icon"></i> </a> </td>
                         </tr>
@@ -29,29 +34,56 @@ function Projects() {
                                 Restaurant Reservation
                                 <p>Full-stack, CRUD, React, Express, PostGreSQL App</p>
                             </td>
+                            <td>
+                                User can Create, Read, Update, and Delete reservations and tables.
+                                Assign party to table with sufficent capacity.
+                                Also have reservation restrictions (business hours, days open, etc)
+                            </td>
                             <td><a href="https://ap-restaurant-reservation-frontend.onrender.com/" className="hvr-pulse-shrink" target="blank"> <i class="fa-solid fa-arrow-up-right-from-square hvr-icon"></i> </a> </td>
                             <td><a href="https://github.com/AlyssaPadayhag/ap-restaurant-reservation" className="hvr-pulse-shrink" target="blank"> <i class="fa-solid fa-arrow-up-right-from-square hvr-icon"></i> </a> </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                CSTCN (Customre Service/Technical Call Notes)
+                                <p>Front-end, React, MUI</p>
+                            </td>
+                            <td>
+                               User input and easily copy to clipboard. Create, Update, Delete in-progress.
+                               Created to make my current job more efficient. i.e. not having to hi-light in a simple notes app vs. clicking to copy
+                            </td>
+                            <td><a href="https://cstcn.onrender.com" className="hvr-pulse-shrink" target="blank"> <i class="fa-solid fa-arrow-up-right-from-square hvr-icon"></i> </a> </td>
+                            <td><a href="https://github.com/AlyssaPadayhag/cstcn" className="hvr-pulse-shrink" target="blank"> <i class="fa-solid fa-arrow-up-right-from-square hvr-icon"></i> </a> </td>
                         </tr>
                         <tr>
                             <td>
                                 WeLoveMovies
                                 <p>Back-end, Node, Express, Knex, PostgreSQL, </p>
                             </td>
+                            <td>
+                            Setting up a database and building out specific routes
+                            so that users can gain access to data about movies, theaters, and reviews. 
+                            See endpoints in code (github) link
+                            </td>
                             <td><a href="https://ap-welovemovies-backend.onrender.com/movies" className="hvr-pulse-shrink" target="blank"> <i class="fa-solid fa-arrow-up-right-from-square hvr-icon"></i> </a> </td>
-                            <td><a href="https://github.com/AlyssaPadayhag/ap-restaurant-reservation" className="hvr-pulse-shrink" target="blank"> <i class="fa-solid fa-arrow-up-right-from-square hvr-icon"></i> </a> </td>
+                            <td><a href="https://github.com/AlyssaPadayhag/ap-welovemovies" className="hvr-pulse-shrink" target="blank"> <i class="fa-solid fa-arrow-up-right-from-square hvr-icon"></i> </a> </td>
                         </tr>
-                        <tr>
+                        {/* <tr>
                             <td>
                                 GrubDash
                                 <p>Back-end, Node, Express, REST API</p>
                             </td>
                             <td></td>
+                            <td></td>
                             <td><a href="https://github.com/AlyssaPadayhag/ap-grubdash" className="hvr-pulse-shrink" target="blank"> <i class="fa-solid fa-arrow-up-right-from-square hvr-icon"></i> </a> </td>
-                        </tr>
+                        </tr> */}
                         <tr>
                             <td>
                                 Notes AWS App
                                 <p>Full-stack, Authentication, AWS: Amplify, S3</p>
+                            </td>
+                            <td>
+                                Example notes app with login. Please feel free to test the account creation and login!
+                                Made with AWS
                             </td>
                             <td><Link to="/notes" target="_blank" className="link-style hvr-pulse-shrink"> <i class="fa-solid fa-arrow-up-right-from-square hvr-icon"></i> </Link></td>
                             <td><a href="https://github.com/AlyssaPadayhag/ap-portfolio/blob/main/src/Notes.js" target="blank"> <i class="fa-solid fa-arrow-up-right-from-square hvr-icon"></i> </a> </td>
@@ -61,6 +93,9 @@ function Projects() {
                                 Simple Landing Page
                                 <p>Front-end design</p>
                             </td>
+                            <td>
+                                Very simple landing page with responsive thumbnail images at the bottom.
+                            </td>
                             <td><a href="https://ap-drinks-landing-page.onrender.com/" className="hvr-pulse-shrink" target="blank"> <i class="fa-solid fa-arrow-up-right-from-square hvr-icon"></i> </a> </td>
                             <td><a href="https://github.com/AlyssaPadayhag/ap-drinks-landing-page" className="hvr-pulse-shrink" target="blank"> <i class="fa-solid fa-arrow-up-right-from-square hvr-icon"></i> </a> </td>
                         </tr>
@@ -68,6 +103,9 @@ function Projects() {
                             <td>
                                 Responsive Flexbox Design
                                 <p>Front-end responsive design</p>
+                            </td>
+                            <td>
+                                Very simple responsive flex design.
                             </td>
                             <td><a href="https://ap-responsive-flexbox.onrender.com/" className="hvr-pulse-shrink" target="blank"> <i class="fa-solid fa-arrow-up-right-from-square"></i> </a> </td>
                             <td><a href="https://github.com/AlyssaPadayhag/ap-responsive-flexbox" className="hvr-pulse-shrink" target="blank"> <i class="fa-solid fa-arrow-up-right-from-square"></i> </a> </td>
