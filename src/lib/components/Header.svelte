b557146621c1ecd3f1a095872165a91f216e25bb
apadayhag <alyssapadayhag@gmail.com>
2026-02-18 16:33:47 -0500

    Add new font and add portfolio image

---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,28 @@
+<div class="main share-tech-regular">
+    <h1>Alyssa Padayhag</h1>
+    <h2>Full Stack Web Developer</h2>
+</div>
+
+
+<style>
+    .share-tech-regular {
+        font-family: "Share Tech", sans-serif;
+        font-weight: 400;
+        font-style: normal;
+    }
+
+    .main {
+        text-align: center;
+        background: #a0c4ab;
+        width: 50%;
+        margin-left: auto;
+        margin-right: auto;
+        border-radius: 15px;
+        margin-top: 15px;
+        padding-bottom: 15px;
+    }
+    h1 {
+        font-size: 48px;
+    }
+
+</style>
