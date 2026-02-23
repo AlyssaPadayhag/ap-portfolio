5cf5549d052e4e55434bb6b43e33705e361e6b3a
apadayhag <alyssapadayhag@gmail.com>
2026-02-23 16:48:36 -0500

    Add background image and some content

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,21 @@
 <script lang="ts">
 	import './layout.css';
 	import favicon from '$lib/assets/favicon.svg';
+	import Header from "$lib/components/Header.svelte";
 
 	let { children } = $props();
 </script>
 
+<Header />
+
+
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
 {@render children()}
+
+
+
+<style>
+	body {
+		background-image: url("src/lib/assets/src/lib/assets/aaron-burden-yzgF-AQt1sQ-unsplash.jpg");
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,32 @@
-<script>
-    import Header from "$lib/components/Header.svelte";
-</script>
 
-<Header />
 
-<img src="src/lib/assets/me.png" alt="Alyssa's portrait">
+<div class="container">
+    <div>
+        <img class="portrait" src="src/lib/assets/me.png" alt="Alyssa's portrait">
+    </div>
+    
+    <div class="infoBox">
+        <h2>Hi there! I'm Alyssa.</h2>
+        <p>Lorem ipsum</p>
+    </div>
+</div>
+
+<style>
+    .container {
+        display: flex;
+        flex-direction: row;
+    }
+    .portrait {
+        width: 40%;
+        margin-left: 50%;
+        margin-right: 0;
+        margin-top: 5%;
+        border-radius: 25px
+    }
+    .infoBox {
+        background-color: #a0c4ab;
+        align-items: left;
+        margin-top: 5%;
+        padding: 10px;
+    }
+</style>
