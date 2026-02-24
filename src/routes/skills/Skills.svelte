74f8cafd5543313c8969c8a2c5ddb075a0fd25ee
apadayhag <alyssapadayhag@gmail.com>
2026-02-24 09:17:45 -0500

    Add Skills content

---
+++ src/routes/skills/Skills.svelte
@@ -0,0 +1,27 @@
+<div class="share-tech-regular">
+    <ul>
+        <li>HTML | CSS | JavaScript | TypeScript</li>
+        <li>Version Control | Git | BitBucket</li>
+        <li>Frameworks | React | Svelte</li>
+        <li>Bootstrap | TailwindCSS</li>
+        <li>SQL | MySQL | Postgres | DBeaver</li>
+        <li>APIs</li>
+        <li>Problem-Solving | Collaboration | Communication</li>
+        <br>
+        <li>Specialties: </li>
+        <li>NetSuite | SuiteScripts | Workflows | Admin</li>
+        <li>BigCommerce | Magento | Shopify | Amazon</li>
+        <br>
+        <li>Other:</li>
+        <li>Languages: Advanced Speaking Tagalog</li>
+    </ul>
+</div>
+
+<style>
+    .share-tech-regular {
+        font-family: "Share Tech", sans-serif;
+        font-weight: 400;
+        font-style: normal;
+        padding: 10px;
+    }
+</style>
