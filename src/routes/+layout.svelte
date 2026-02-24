13fd4415a413d743d67d11b4592e66ccee015c19
apadayhag <alyssapadayhag@gmail.com>
2026-02-24 09:18:30 -0500

    Add Footer component to render

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,7 @@
 	import './layout.css';
 	import favicon from '$lib/assets/favicon.svg';
 	import Header from "$lib/components/Header.svelte";
+	import Footer from '$lib/components/Footer.svelte';
 
 	let { children } = $props();
 </script>
@@ -11,3 +12,5 @@
 
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
 {@render children()}
+
+<Footer />
