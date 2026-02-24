3b6c5963258ee7a815ff47329aaa3bf1a008ac5d
apadayhag <alyssapadayhag@gmail.com>
2026-02-24 08:31:59 -0500

    Add styles and add navbar links

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,32 +1,54 @@
+<script>
+    import Home from "./home/Home.svelte";
 
-
+    export let menu = 1;
+</script>
+<ul id="menu" class="topnav">
+	<li><a href="/" on:click|preventDefault={() => (menu = 1)}>Beranda</a></li>
+	<li><a href="/" on:click|preventDefault={() => (menu = 2)}>Profile</a></li>
+</ul>
 <div class="container">
-    <div>
-        <img class="portrait" src="src/lib/assets/me.png" alt="Alyssa's portrait">
-    </div>
-    
-    <div class="infoBox">
-        <h2>Hi there! I'm Alyssa.</h2>
-        <p>Lorem ipsum</p>
+    <div class="portrait">
+        <img src="src/lib/assets/me.png" alt="Alyssa's portrait">
     </div>
 </div>
 
 <style>
     .container {
         display: flex;
-        flex-direction: row;
+        justify-content: left;
     }
     .portrait {
-        width: 40%;
-        margin-left: 50%;
-        margin-right: 0;
-        margin-top: 5%;
-        border-radius: 25px
+        width: 20%;
+        height: auto;
+        margin-left: 30%;
+        border-radius: 25px;
     }
-    .infoBox {
+    .content {
+        margin-right: 30%;
         background-color: #a0c4ab;
-        align-items: left;
-        margin-top: 5%;
-        padding: 10px;
+        text-align: center;
     }
+    img {
+        border-bottom-left-radius: 15px;
+        border-bottom-right-radius: 15px;
+    }
+    ul#menu li {
+        display: inline;
+    }
+	.topnav {
+		background-color: white;
+		overflow: hidden;
+		width: 50%;
+		margin-left: auto;
+		margin-right: auto;
+	}
+	.topnav li a {
+		float: left;
+		color: red;
+		text-align: center;
+		padding: 14px 16px;
+		text-decoration: none;
+		font-size: 17px;
+	}
 </style>
