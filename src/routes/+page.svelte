822b721e7ad874572a77b700b1758815502fd507
apadayhag <alyssapadayhag@gmail.com>
2026-02-24 08:57:47 -0500

    Add styling and Nav logic

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,29 @@
 <script>
     import Home from "./home/Home.svelte";
+    import Skills from "./skills/Skills.svelte";
 
     export let menu = 1;
 </script>
+
 <ul id="menu" class="topnav">
-	<li><a href="/" on:click|preventDefault={() => (menu = 1)}>Beranda</a></li>
-	<li><a href="/" on:click|preventDefault={() => (menu = 2)}>Profile</a></li>
+	<li><a href="/" on:click|preventDefault={() => (menu = 1)}>01. Home</a></li>
+	<li><a href="/" on:click|preventDefault={() => (menu = 2)}>02. Skills</a></li>
 </ul>
+
 <div class="container">
     <div class="portrait">
         <img src="src/lib/assets/me.png" alt="Alyssa's portrait">
+    </div>
+    <div class="content">
+        {#if menu === 1}
+        <Home />
+        {:else if menu === 2}
+        <Skills />
+        {:else}
+        <h1>
+            Page Not Found
+        </h1>
+        {/if}
     </div>
 </div>
 
@@ -19,19 +33,16 @@
         justify-content: left;
     }
     .portrait {
-        width: 20%;
+        width: 30%;
         height: auto;
         margin-left: 30%;
         border-radius: 25px;
     }
     .content {
+        width: 50%;
         margin-right: 30%;
         background-color: #a0c4ab;
         text-align: center;
-    }
-    img {
-        border-bottom-left-radius: 15px;
-        border-bottom-right-radius: 15px;
     }
     ul#menu li {
         display: inline;
