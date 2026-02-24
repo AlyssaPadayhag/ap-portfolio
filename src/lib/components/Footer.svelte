e2b5833332e951c3b371f216b2c9553a5bf6700f
apadayhag <alyssapadayhag@gmail.com>
2026-02-24 09:19:54 -0500

    Create a Footer component

---
+++ src/lib/components/Footer.svelte
@@ -0,0 +1,27 @@
+<div class="main share-tech-regular">
+    <h1>Let's connect!</h1>
+</div>
+
+<style>
+
+    .main {
+        text-align: center;
+        background: #a0c4ab;
+        width: 50%;
+        margin-left: auto;
+        margin-right: auto;
+        border-bottom-left-radius: 15px;
+        border-bottom-right-radius: 15px;
+        margin-bottom: 15px;
+        padding-bottom: 15px;
+    }
+    .share-tech-regular {
+        font-family: "Share Tech", sans-serif;
+        font-weight: 400;
+        font-style: normal;
+    }
+    h1 {
+        font-size: 48px;
+    }
+
+</style>
