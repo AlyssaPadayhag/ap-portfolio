c5f4e2ec7e1264b1d7f47b2e80e393d3ef79c6b9
apadayhag <alyssapadayhag@gmail.com>
2026-02-24 08:56:18 -0500

    Add content to the Home link

--- src/routes/home/Home.svelte
+++ src/routes/home/Home.svelte
@@ -1,1 +1,21 @@
-<h1>Testing</h1>
+<div class="share-tech-regular">
+    <h1>Hi There!</h1>
+    <p>I am a full stack web developer~</p>
+    <br>
+    <p>My interest in web dev first started when I was a young girl, in the MySpace era, where we can modify our page
+        to make it look how we want.
+    </p>
+    <p>I also grew up with four boys and we all had a great interest in video games, which led me to my curiosity
+        of how computers and software work.
+    </p>
+    <br>
+    <p>Now here I am, always learning and always looking for the next challenge~</p>
+</div>
+
+<style>
+    .share-tech-regular {
+        font-family: "Share Tech", sans-serif;
+        font-weight: 400;
+        font-style: normal;
+    }
+</style>
