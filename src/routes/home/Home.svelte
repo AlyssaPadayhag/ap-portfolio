90c52a486d0e06ea9c2af2349375f9c6b5226006
Alyssa <alyssapadayhag@gmail.com>
2026-04-19 18:56:36 -0400

    Fix NetSuite link issue

--- src/routes/home/Home.svelte
+++ src/routes/home/Home.svelte
@@ -1,13 +1,23 @@
 <div class="share-tech-regular">
-    <h1>Hi There!</h1>
-    <p>Based in Dayton, OH.</p>
-    <p>My interest in web dev first started when I was a young girl, in the MySpace era, where we can modify our page
-        to make it look how we want.
-    </p>
-    <p>I also grew up with four boys and we all had a great interest in video games and computers, which led me to my curiosity
-        of how computers and software work.
-    </p>
 
+    <h1>NetSuite SuiteScript Projects</h1>
+    <hr>
+    <ul>
+        <li>Implementation completed in under a year</li>
+        <li>Implementation of PaceJet/Descartes, third party app integration</li>
+        <li>Many custom workflows</li>
+        <li>Custom excise tax workflow created</li>
+        <li>Custom shipping labels</li>
+        <li>Custom PDF files (Invoices, Sales Orders, Packing Slip, etc)</li>
+        <li>Custom PDF worflows using Freemaker</li>
+        <li>Custom shipping prices based on zip codes for internal delivery service</li>
+        <li>Custom internal POS system to quicken Pick and Pull process</li>
+        <li>Custom Lot Assembly Creation from the in-house clean room procedure</li>
+        <li>Connection to Magento using NetSuite Connector (formerly known as FarApp)</li>
+        <li>Data Analysis</li>
+        <li>Consulting</li>
+        <li>Admin Work</li>
+    </ul>
 
 </div>
 
@@ -17,5 +27,8 @@
         font-weight: 400;
         font-style: normal;
         padding: 10px;
+    }
+    li {
+        justify-content: left;
     }
 </style>
